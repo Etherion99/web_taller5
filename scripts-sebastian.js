f2115318810f0e5238a378503b3ec8b8e1ba3cf7
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-09-07 08:47:26 -0500

    basic validation example

--- scripts-sebastian.js
+++ scripts-sebastian.js
@@ -2,11 +2,11 @@ const inputs = document.querySelectorAll('.form-control, .form-select');
 
 for (let input of inputs) {
     input.addEventListener('focus', (event) => {
-        input.parentNode.parentNode.querySelector('label').style.color = '#F44336';
+        input.parentNode.parentNode.querySelector('label').style.color = '#3949AB';
     });
 
     input.addEventListener('blur', (event) => {
-        input.parentNode.parentNode.querySelector('label').style.color = 'black';
+        input.parentNode.parentNode.querySelector('label').style.color = '#8B8B8B';
     });
 }
 
@@ -16,6 +16,23 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const formData = new FormData(form);
+    const formObj = Object.fromEntries(formData);
 
-    console.log(formData);
+    const name = document.getElementById('name');
+    const lastname = document.getElementById('lastname');
+
+    validation(name, formObj['name'] == '', 'Campo obligatorio');
+    validation(lastname, formObj['lastname'] == '', 'Campo obligatorio');
 });
+
+function validation(input, condition, message) {
+    if (condition) {
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        input.parentNode.querySelector('.invalid-feedback').innerText = message;
+    } else {
+        input.classList.add('is-valid');
+        input.classList.remove('is-invalid');
+        input.parentNode.querySelector('.invalid-feedback').innerText = '';
+    }
+}
